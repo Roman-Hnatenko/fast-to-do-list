5dcab05c0ab65e2455ca45c78f89a34bded4835c
Roman Hnatenko <hnatenkoroman@gmail.com>
2022-07-08 13:58:19 +0300

    Fix endpoint  create user

--- src/api/users/views.py
+++ src/api/users/views.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Body, Depends, status
+from fastapi import APIRouter, Body, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-
+from sqlalchemy.exc import IntegrityError
 from api.dependencies import get_db
 
 from .models import UserInput, UserOutput
@@ -11,4 +11,11 @@ users_router = APIRouter(prefix='/user')
 
 @users_router.post('/sign_up', status_code=status.HTTP_201_CREATED, response_model=UserOutput)
 async def sign_up(user: UserInput = Body(), db: Session = Depends(get_db)):
-    return create_user(db, user)
+    try:
+        return create_user(db, user)
+    except IntegrityError as error:
+        email = error.params['email']
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f'User with email {email} already exists',
+        )
