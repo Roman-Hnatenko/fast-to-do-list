6a50a574a8eb3338027a53f07602568f01e7de87
Roman Hnatenko <hnatenkoroman@gmail.com>
2022-07-12 15:05:05 +0300

    Add caching user data using Redis DB

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,5 +1,10 @@
 
-from fastapi import Depends
+from datetime import timedelta
+from typing import AsyncGenerator
+
+from aioredis import Redis
+from fastapi import BackgroundTasks, Depends
+from fastapi.encoders import jsonable_encoder
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from sqlalchemy import select
@@ -12,6 +17,12 @@ from .database import async_session
 from .exceptions import HttpUnauthorized
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+
+async def get_redis() -> AsyncGenerator[Redis, None]:
+    redis = await Redis(decode_responses=True)
+    yield redis
+    await redis.close()
 
 
 async def get_session() -> AsyncSession:
@@ -30,8 +41,22 @@ async def require_auth(token: str = Depends(oauth2_scheme)):
     return email
 
 
+async def add_user_to_redis(redis: Redis, user: UserModel):
+    data = jsonable_encoder(user)
+    await redis.hmset(user.email, data)
+    await redis.expire(user.email, timedelta(minutes=10))
+
+
 async def get_current_user(
-    email: str = Depends(require_auth), session: AsyncSession = Depends(get_session),
+    background_tasks: BackgroundTasks,
+    email: str = Depends(require_auth),
+    session: AsyncSession = Depends(get_session),
+    redis: Redis = Depends(get_redis),
 ) -> UserModel:
+    if cached_user := await redis.hgetall(email):
+        cached_user['id'] = int(cached_user['id'])
+        return UserModel(**cached_user)
     result = await session.execute(select(UserModel).where(UserModel.email == email))
-    return result.scalar()
+    user = result.scalar()
+    background_tasks.add_task(add_user_to_redis, redis, user)
+    return user
